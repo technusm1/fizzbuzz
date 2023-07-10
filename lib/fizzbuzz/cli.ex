9705c96682a5afc9e92b447075974ff5a286aa45
Maheep Kumar Kathuria <me@maheepk.net>
2023-07-10 17:02:05 +0530

    Special handling for chunk_size == 6000 increases throughput to 175 MiB/s

--- lib/fizzbuzz/cli.ex
+++ lib/fizzbuzz/cli.ex
@@ -1,16 +1,63 @@
 defmodule Fizzbuzz.Cli do
   def main([lower, upper]) do
     {lower, upper} = {String.to_integer(lower), String.to_integer(upper)}
-    chunk_size = min(div(upper - lower, System.schedulers_online), 6000)
-    input_enumerable = get_input_ranges2(lower, upper, chunk_size)
-    Task.async_stream(input_enumerable, fn input -> elem(GenServer.start_link(Fizzbuzz.Worker, [input]), 1) end, timeout: :infinity)
-    |> Stream.map(fn {:ok, res} -> res end)
-    |> Stream.each(
-      fn pid ->
+    chunk_size = min(div(upper - lower, System.schedulers_online()), 6000)
+
+    if chunk_size == 6000 do
+      # We'll divide the input range into 3 parts: beginning, 6k ranges and ending
+      # beginning and ending will be processed before and after stream.run respectively.
+      input_lower =
+        case rem(lower, 15) do
+          1 ->
+            lower
+
+          0 ->
+            IO.binwrite("FizzBuzz\n")
+            lower + 1
+
+          remainder ->
+            IO.binwrite(Fizzbuzz.fizzbuzz_no_io(lower..(15 - remainder + lower)))
+            15 - remainder + lower + 1
+        end
+
+      input_upper =
+        case rem(upper - input_lower + 1, 6000) do
+          0 -> upper
+          remainder -> upper - remainder
+        end
+
+      input_enumerable = Chunk6kStream.create(input_lower..input_upper)
+
+      Task.async_stream(
+        input_enumerable,
+        fn input -> elem(GenServer.start_link(Fizzbuzz.Worker, [input]), 1) end,
+        timeout: :infinity
+      )
+      |> Stream.map(fn {:ok, res} -> res end)
+      |> Stream.each(fn pid ->
         GenServer.call(pid, :print)
         Process.exit(pid, :kill)
       end)
-    |> Stream.run()
+      |> Stream.run()
+
+      if input_upper < upper do
+        IO.binwrite(Fizzbuzz.fizzbuzz_no_io(input_upper+1..upper))
+      end
+    else
+      input_enumerable = get_input_ranges2(lower, upper, chunk_size)
+
+      Task.async_stream(
+        input_enumerable,
+        fn input -> elem(GenServer.start_link(Fizzbuzz.Worker, [input]), 1) end,
+        timeout: :infinity
+      )
+      |> Stream.map(fn {:ok, res} -> res end)
+      |> Stream.each(fn pid ->
+        GenServer.call(pid, :print)
+        Process.exit(pid, :kill)
+      end)
+      |> Stream.run()
+    end
   end
 
   def main(_), do: IO.puts("Usage: fizzbuzz 1 10000")
@@ -25,24 +72,48 @@ defmodule Fizzbuzz.Cli do
   end
 end
 
+defmodule Chunk6kStream do
+  # Make sure that range has size of multiples of 6000 and range.first is divisible by 15
+  def create(range) do
+    Stream.resource(fn -> initialize(range) end, &generate_next_value/1, &done/1)
+  end
+
+  defp initialize(range) do
+    {range, range.first, range.last}
+  end
+
+  defp generate_next_value({range, lower, upper}) when lower == upper + 1 do
+    {:halt, {range, upper, upper}}
+  end
+
+  defp generate_next_value({range, lower, upper}) do
+    {[lower..(lower + 5999)], {range, lower + 6000, upper}}
+  end
+
+  defp done(_) do
+    nil
+  end
+end
+
 defmodule ChunkRangeStream do
-	def create(range, chunk_size) do
-		Stream.resource(fn -> initialize(range, chunk_size) end, &generate_next_value/1, &done/1)
-	end
+  def create(range, chunk_size) do
+    Stream.resource(fn -> initialize(range, chunk_size) end, &generate_next_value/1, &done/1)
+  end
 
-	defp initialize(range, chunk_size) do
-		{range, chunk_size, range.first}
-	end
+  defp initialize(range, chunk_size) do
+    {range, chunk_size, range.first}
+  end
 
-	defp generate_next_value({range, chunk_size, lower}) do
+  defp generate_next_value({range, chunk_size, lower}) do
     if lower < range.last do
-      {[lower..min(lower+chunk_size, range.last)], {range, chunk_size, min(range.last, lower+chunk_size+1)}}
+      {[lower..min(lower + chunk_size, range.last)],
+       {range, chunk_size, min(range.last, lower + chunk_size + 1)}}
     else
       {:halt, {range, chunk_size, lower}}
     end
-	end
+  end
 
-	defp done(_) do
-		nil
-	end
+  defp done(_) do
+    nil
+  end
 end
--- lib/fizzbuzz/worker.ex
+++ lib/fizzbuzz/worker.ex
@@ -7,7 +7,17 @@ defmodule Fizzbuzz.Worker do
   end
 
   def handle_info({:calculate, range}, _state) do
-    res = Fizzbuzz.fizzbuzz_no_io(range)
+    res = if Range.size(range) == 6000 do
+      i = range.first - 1
+      0..(400-1)
+      |> Stream.map(fn j ->
+        [[Integer.to_string(15 * j + 1 + i), "\n"], [Integer.to_string(15 * j + 2 + i), "\n"], "Fizz\n", [Integer.to_string(15 * j + 4 + i), "\n"], "Buzz\nFizz\n", [Integer.to_string(15 * j + 7 + i), "\n"], [Integer.to_string(15 * j + 8 + i), "\n"], "Fizz\nBuzz\n", [Integer.to_string(15 * j + 11 + i), "\n"], "Fizz\n", [Integer.to_string(15 * j + 13 + i), "\n"], [Integer.to_string(15 * j + 14 + i), "\n"], "FizzBuzz\n"]
+      end)
+      |> Stream.chunk_every(400)
+      |> Enum.into([])
+    else
+      Fizzbuzz.fizzbuzz_no_io(range)
+    end
     {:noreply, res}
   end
 
